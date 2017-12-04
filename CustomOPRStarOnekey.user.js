1ee1afbc25734decf01df6e8a2f15af3352c6ee2
XiFanU <31371729+xifanu@users.noreply.github.com>
2017-12-04 14:14:17 +0800

    Update CustomOPRStarOnekey.user.js

--- CustomOPRStarOnekey.user.js
+++ CustomOPRStarOnekey.user.js
@@ -1,6 +1,13 @@
 // ==UserScript==
 // @name         CustomStarOfOpr
 // @version      0.25.1
+// @description  Give five star with single click
+// @updateURL    https://github.com/jqqqqqqqqqq/MakeOPRGreatAgain/raw/master/5StarOneKey.user.js
+// @downloadURL  https://github.com/jqqqqqqqqqq/MakeOPRGreatAgain/raw/master/5StarOneKey.user.js
+// @author       jqqqqqqqqqq
+// @match        https://opr.ingress.com/recon
+// @grant        none
+// ==/UserScript==
 var buttons = [
 	{button:"Five Star", total:5, name:5, history:5, unique:5, location:5, safety:5},
 	{button:"553355", total:5, name:5, history:3, unique:3, location:5, safety:5},
